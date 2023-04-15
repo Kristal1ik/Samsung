6f883cc92698e2f06d780465e6ac4eb05ec3602b
god-of-jesters <ryzovdima@gmail.com>
2023-04-16 01:20:58 +0300

    SingUp

--- app/src/main/java/com/example/samsung/SignUp.java
+++ app/src/main/java/com/example/samsung/SignUp.java
@@ -1,5 +1,6 @@
 package com.example.samsung;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -10,12 +11,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.samsung.BD.RetrofitUserServ;
+import com.example.samsung.BD.User;
 import com.example.samsung.databinding.FragmentSignupBinding;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 public class SignUp extends Fragment {
     FragmentSignupBinding binding;
-
+    String[] names = new String[10];
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,7 +48,54 @@ public class SignUp extends Fragment {
                 String password = binding.passwordSignup.getText().toString();
                 String confirm = binding.againPasswordSignup.getText().toString();
                 binding.usernameSignup.setText(""); binding.passwordSignup.setText(""); binding.againPasswordSignup.setText("");
+                takeName();
+                System.out.println(names[0]);
+                if(log(username, names)){
+//                    переход сделать
+//                    if(password.equals(confirm)){
+////                        переход
+//                    }else{
+//                        //                    обработать
+//                    }
+                }else{
+//                    обработать
+                }
+
+
             }
         });
+    }
+
+    public void takeName(){
+        RetrofitUserServ.getInstance().getUsers().enqueue(new Callback<List<User>>() {
+            @Override
+            public void onResponse(Call<List<User>> call, Response<List<User>> response) {
+                assert response.body() != null;
+                for (User user: response.body()){
+                    String name = user.getName();
+                    System.out.println(name);
+                    if(name != null)
+                        names[0] = name;
+                    else {
+                        names[0] = "ksefv";
+                    }
+                    System.out.println(names[0]);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<User>> call, Throwable t) {
+                t.printStackTrace();
+            }
+        });
+    }
+
+    public Boolean log(String name, String[] names){
+        names[0] = "efkl";
+        if(names[0].equals(name)){
+            return false;
+        }else{
+            return true;
+        }
     }
 }
