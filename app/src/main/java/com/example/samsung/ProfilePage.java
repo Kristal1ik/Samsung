bc1291bb547734e3198a683c8e07ed92c1a6b2af
Kristal1ik <90793805+Kristal1ik@users.noreply.github.com>
2023-04-15 17:21:42 +0300

    eee

--- app/src/main/java/com/example/samsung/ProfilePage.java
+++ app/src/main/java/com/example/samsung/ProfilePage.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.samsung.databinding.FragmentLoginBinding;
+
 public class ProfilePage extends AppCompatActivity {
+    FragmentLoginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
