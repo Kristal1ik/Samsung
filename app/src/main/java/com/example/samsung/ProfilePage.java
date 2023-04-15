86e589a429b72ff6585682080d7b9f19e6af9b87
Kristal1ik <90793805+Kristal1ik@users.noreply.github.com>
2023-04-16 01:15:24 +0300

    friend

--- app/src/main/java/com/example/samsung/ProfilePage.java
+++ app/src/main/java/com/example/samsung/ProfilePage.java
@@ -5,20 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.samsung.databinding.FragmentLoginBinding;
 
 public class ProfilePage extends AppCompatActivity {
-    FragmentLoginBinding binding;
-
+    TextView btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_page);
+        btn = (TextView) findViewById(R.id.places);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfilePage.this, Friend.class);
+                startActivity(intent);
+            }
+        });
     }
-    public void get_places(View view) {
-        Intent intent = new Intent(ProfilePage.this, Map.class);
-        startActivity(intent);
-    }
+
+
 
 }
